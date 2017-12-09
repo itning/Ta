8dd7576855390557139ea6ffd130dde5c33ed034
ITNING <3579677768@qq.com>
2017-12-09 18:05:34 +0800

    登录信息添加日志输出

--- src/main/java/top/itning/ta/config/UserDetailsServiceImpl.java
+++ src/main/java/top/itning/ta/config/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package top.itning.ta.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,13 +15,17 @@ import top.itning.ta.entity.User;
  */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
     @Autowired
     private UserDao userDao;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.debug("loadUserByUsername::开始登陆,用户名->" + username);
         User user = userDao.findByUsername(username);
         if (user == null) {
+            logger.warn("loadUserByUsername::用户名:" + username + "不存在");
             throw new UsernameNotFoundException("用户名:" + username + "不存在");
         }
         return user;
